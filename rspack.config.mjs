581c4c1e2d9187a1f959dc424b8a5b138bab4e9b
Egor Blinov <egor.b@miro.com>
2025-07-23 22:20:34 +0200

    replicate with pure rspack/webpack

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -29,6 +29,42 @@ const config = {
   experiments: {
     css: true,
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        critical: {
+          /*
+              Bug/feature-request #1
+              I'm trying to split modules with "critial" in the name into single seprate chunks.
+              One critical chunk for each js chunk
+              That works as expected with `name: undefined`
+              But as soon as I want to mark these chunks with some suffix, like `[chunkId].critical`, problems begin
+              I cannot replicate the `undefined` behavior:
+                - if I return `name: 'critical`, I get single critical chunk for all my dynamic chunks
+                - if I use callback, the chunks argument has no id for dynamic chunks, until I gave it a "webpackChunkName" magic comment.
+
+              The last option solves it for me, but I'd prefer not to think about adding magic comments each time I do `import()`.
+              Whats more important, I'd like to solve this problem without rust/js interop, preferrably with somehting like `name: '[parentChunkId].critical'`
+              I tried using `idHint: 'critical'`, but it doesn't seem to work at all, maybe I misunderstood what it is supposed to do.
+            */
+          name: (module, chunks) => {
+            if (!chunks[0].name) {
+              throw new Error("give dynamic chunk a name!");
+            }
+            return `${chunks[0].name}.critical`;
+          },
+          // could be a solution, according to doc, but makes the chunk names unexpectedly long
+          // idHint: "critical",
+          chunks: "all",
+          /* Bug #2: the regexp doesn't work for module.type === 'css/mini-extract' */
+          // test: /critical/,
+          // but somehow, the identifier is available if called inside callback
+          test: (module) => module.identifier().includes("critical"),
+          enforce: true,
+        },
+      },
+    },
+  },
 };
 
 export default config;
